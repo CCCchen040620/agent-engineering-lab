8f636e5a11eaedbbf74edda604382e45c5da9def
CCCchen040620 <2627559658@qq.com>
2026-07-04 14:53:18 +0800

    feat: practice function parameters and return value

---
+++ week01/function_practice.py
@@ -0,0 +1,9 @@
+def calculate_tatal_hours(weekly_hours,total_weeks):
+    total_hours = weekly_hours * total_weeks
+    return total_hours
+
+hours = int(input("你每周计划学习多少小时？"))
+weeks = int(input("你计划学习多少周？"))
+result = calculate_tatal_hours(hours,weeks)
+
+print(weeks,"周一共学习",result,"小时")
