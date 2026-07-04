d7cec9568720df451a22974a5fe0e212220d69b8
CCCchen040620 <2627559658@qq.com>
2026-07-04 14:33:31 +0800

    feat: handle invalid weekly hours input

--- week01/interactive_profile.py
+++ week01/interactive_profile.py
@@ -1,7 +1,11 @@
 name = input("请输入你的名字：")
 weekly_hours = input("你每周计划学习多少个小时？")
 
-total_hours = int(weekly_hours) * 20
+try:
+    total_hours = int(weekly_hours) * 20
+    print("你好，",name)
+    print("按照你的计划，20周一共学习",total_hours,"小时")
+except ValueError:
+    print("学习时间请输入数字，比如6或8。")
 
-print("你好，",name)
-print("按照你的计划，20周一共学习",total_hours,"小时")
+
