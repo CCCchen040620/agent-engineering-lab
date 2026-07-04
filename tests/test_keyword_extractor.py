e7863c3c72d1d64da1775df839c212bceea5b385
CCCchen040620 <2627559658@qq.com>
2026-07-04 21:57:02 +0800

    feat: prefer specific extracted keywords

--- tests/test_keyword_extractor.py
+++ tests/test_keyword_extractor.py
@@ -2,7 +2,7 @@ from week03.keyword_extractor import extract_keyword
 
 
 def test_extract_keyword_finds_reimbursement():
-    keyword = extract_keyword("差旅报销多久内提交？")
+    keyword = extract_keyword("普通报销需要什么材料？")
 
     assert keyword == "报销"
 
@@ -17,3 +17,9 @@ def test_extract_keyword_returns_original_question_when_unknown():
     keyword = extract_keyword("公司有没有股票期权？")
 
     assert keyword == "公司有没有股票期权？"
+
+
+def test_extract_keyword_prefers_more_specific_keyword():
+    keyword = extract_keyword("差旅报销多久内提交？")
+
+    assert keyword == "差旅报销"
--- week03/keyword_extractor.py
+++ week03/keyword_extractor.py
@@ -1,5 +1,13 @@
 def extract_keyword(question):
-    known_keywords = ["安全培训", "报销", "请假", "年假", "病假", "弹性工作制"]
+    known_keywords = [
+        "差旅报销",
+        "安全培训",
+        "报销",
+        "请假",
+        "年假",
+        "病假",
+        "弹性工作制",
+    ]
 
     for keyword in known_keywords:
         if keyword in question:
